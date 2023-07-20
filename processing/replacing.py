fd987a46b0668bd75b687f02aa695d291af5af91
Maximilian H <lorganan@gmail.com>
2023-07-20 20:40:00 +0200

    some fixes using regex

--- processing/replacing.py
+++ processing/replacing.py
@@ -9,11 +9,11 @@ for file in file_list:
     file = file.split("\\")[-1].split(".")[0]
     with open(path.join(dir, "voyages_text", file + ".txt"), "r") as input:
         string = input.read()
-    str_to_replace = " Mohcli "
-    str_new = " Hugli "
+    str_to_replace = " LSaugor "
+    str_new = " Saugor "
     occurences_list = re.findall(str_to_replace, string)
     occurences += len(occurences_list)
     string = string.replace(str_to_replace, str_new)
     with open(path.join(dir, "voyages_text", file + ".txt"), "w") as output:
         output.write(string)
-print("\"", str_to_replace + "\" to \"" + str_new + "\":", occurences)
+print("\"" + str_to_replace + "\" to \"" + str_new + "\":", occurences)
