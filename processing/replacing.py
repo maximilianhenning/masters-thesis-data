df4ebf31ea2ff05ad9b7879e53109c8ddcade3c0
Maximilian H <lorganan@gmail.com>
2023-07-20 20:37:15 +0200

    some fixes using regex

--- processing/replacing.py
+++ processing/replacing.py
@@ -4,14 +4,16 @@ import re
 
 dir = path.dirname(__file__)
 file_list = glob(path.join(dir, "voyages_text", "*"))
+occurences = 0
 for file in file_list:
     file = file.split("\\")[-1].split(".")[0]
     with open(path.join(dir, "voyages_text", file + ".txt"), "r") as input:
         string = input.read()
-    str_to_replace = "Tellicheny"
-    str_new = "Tellicherry"
-    occurences = re.findall(str_to_replace, str)
-    print(len(occurences))
-    string.replace(str_to_replace, str_new)
-    with open(path.join(dir, "voyages_text", file + ".txt"), "r") as output:
+    str_to_replace = " Mohcli "
+    str_new = " Hugli "
+    occurences_list = re.findall(str_to_replace, string)
+    occurences += len(occurences_list)
+    string = string.replace(str_to_replace, str_new)
+    with open(path.join(dir, "voyages_text", file + ".txt"), "w") as output:
         output.write(string)
+print("\"", str_to_replace + "\" to \"" + str_new + "\":", occurences)
