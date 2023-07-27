e4b4926195d441556882feae43fd28d0ca487750
Maximilian H <lorganan@gmail.com>
2023-07-27 18:48:06 +0200

    reran analysis

--- scripts/replacing.py
+++ scripts/replacing.py
@@ -9,11 +9,11 @@ for file in file_list:
     file = file.split("\\")[-1].split(".")[0]
     with open(path.join(dir, "..", "processing", "voyages_text", file + ".txt"), "r") as input:
         string = input.read()
-    str_to_replace = " Starits of Sunda "
-    str_new = " Straits of Sunda "
+    str_to_replace = "Sind "
+    str_new = "Sindh "
     occurences_list = re.findall(str_to_replace, string)
     occurences += len(occurences_list)
     string = string.replace(str_to_replace, str_new)
-    with open(path.join(dir, "voyages_text", file + ".txt"), "w") as output:
+    with open(path.join(dir, "..", "processing", "voyages_text", file + ".txt"), "w") as output:
         output.write(string)
 print("\"" + str_to_replace + "\" to \"" + str_new + "\":", occurences)
